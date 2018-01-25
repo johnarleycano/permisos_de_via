857cd0f8d5dc54385598542e31fd38a9adad0451
John Arley Cano Salinas <johnarleycano@hotmail.com>
2018-01-25 15:58:52 -0500

    [0.12.2] style(general): nombramiento de versión

---
+++ application/views/core/footer.php
@@ -0,0 +1,12 @@
+<footer style="
+    background-color: #808080;
+    bottom: 0;
+    color: white;
+    font-size: 0.7em;
+    left: 0;
+	position: fixed;
+    text-align: center;
+    width: 100%;
+">
+	Sistema de administración de permisos de usos de vía | Devimed S.A. | <i>Versión 0.12.2</i>
+</footer>
