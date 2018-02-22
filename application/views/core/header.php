460138348b666ba13b715e787525ef645a34a3f2
John Arley <johnarleycano@hotmail.com>
2018-02-22 11:52:31 -0500

    [1.08.00] feat(general): Instalado íconos Font Awesome

--- application/views/core/header.php
+++ application/views/core/header.php
@@ -5,27 +5,30 @@
 <!-- Título que viene desde el controlador de cada interfaz -->
 <title><?php echo $titulo; ?> | Permisos de uso de vía</title>
 
-<!-- Estilos -->
-<link rel="stylesheet" href="<?php echo base_url(); ?>css/uikit.min.css" />
-<link rel="stylesheet" href="<?php echo base_url(); ?>css/estilos.css" />
-
-
 <script src="<?php echo base_url(); ?>js/jquery-3.2.1.min.js"></script>
 
 <?php if(ENVIRONMENT === 'development') { ?>
 	<!-- Estilos -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/fontawesome-all.css" />
 
 	<!-- Scripts -->
 	<script src="<?php echo base_url(); ?>js/uikit.js"></script>
 	<script src="<?php echo base_url(); ?>js/uikit-icons.js"></script>
+	<script src="<?php echo base_url(); ?>js/fontawesome-all.js"></script>
 <?php } ?>
 
 <?php if(ENVIRONMENT === 'production') { ?>
 	<!-- Estilos -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/fontawesome-all.min.css" />
 
 	<!-- Scripts -->
 	<script src="<?php echo base_url(); ?>js/uikit.min.js"></script>
 	<script src="<?php echo base_url(); ?>js/uikit-icons.min.js"></script>
+	<script src="<?php echo base_url(); ?>js/fontawesome-all.min.js"></script>
 <?php } ?>
+
+<!-- Estilos -->
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/uikit.min.css" />
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/estilos.css" />
 
 <script src="<?php echo base_url(); ?>js/funciones.js"></script>
